5d0c6b99dca98b895149ed10adbd40e1cdef5b5f
Ilya Belyavskiy <iabelyavskiy@cloud.ru>
2026-03-05 19:54:14 +0300

    fix(FF-8024): translations for Dropdown

--- packages/locale/src/locales/en_GB.ts
+++ packages/locale/src/locales/en_GB.ts
@@ -1,6 +1,22 @@
 import { Dictionary } from '../types';
 
 export const en_GB = {
+  Dropdown: {
+    states: {
+      notFound: {
+        title: 'Not Found',
+        action: 'Update',
+      },
+      noData: {
+        title: 'No data',
+        action: 'Update',
+      },
+      dataError: {
+        title: 'Data error',
+        action: 'Update',
+      },
+    },
+  },
   SearchPrivate: {
     placeholder: 'Search',
   },
--- packages/locale/src/locales/ru_RU.ts
+++ packages/locale/src/locales/ru_RU.ts
@@ -1,6 +1,22 @@
 import { en_GB } from './en_GB';
 
 export const ru_RU: typeof en_GB = {
+  Dropdown: {
+    states: {
+      notFound: {
+        title: 'Не найдено',
+        action: 'Перезагрузить',
+      },
+      noData: {
+        title: 'Нет данных',
+        action: 'Перезагрузить',
+      },
+      dataError: {
+        title: 'Ошибка',
+        action: 'Перезагрузить',
+      },
+    },
+  },
   SearchPrivate: {
     placeholder: 'Поиск',
   },
