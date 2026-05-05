88b329c11b791bb86a260405b6266a3a477a7780
Ilya Belyavskiy <iabelyavskiy@cloud.ru>
2026-05-05 19:20:03 +0300

    fix(FF-8405): calendar locales

--- packages/locale/src/locales/en_GB.ts
+++ packages/locale/src/locales/en_GB.ts
@@ -1,6 +1,27 @@
 import { Dictionary } from '../types';
 
 export const en_GB = {
+  Calendar: {
+    current: 'Current',
+    apply: 'Apply',
+    time: 'Time',
+    presets: 'Presets',
+    prevPeriodMonth: 'Previous month',
+    nextPeriodMonth: 'Next month',
+    prevPeriodYear: 'Previous year',
+    nextPeriodYear: 'Next year',
+    prevPeriodDecade: 'Previous decade',
+    nextPeriodDecade: 'Next decade',
+    defaultPresets: {
+      lastWeek: 'Last 7 days',
+      lastTwoWeeks: 'Last 14 days',
+      lastMonth: 'Last 30 days',
+      lastQuarter: 'Last 90 days',
+      lastThird: 'Last 120 days',
+      lastYear: 'Last 1 year',
+      lastTwoYears: 'Last 2 years',
+    },
+  },
   Dropdown: {
     states: {
       notFound: {
--- packages/locale/src/locales/ru_RU.ts
+++ packages/locale/src/locales/ru_RU.ts
@@ -1,6 +1,27 @@
 import { en_GB } from './en_GB';
 
 export const ru_RU: typeof en_GB = {
+  Calendar: {
+    current: 'Сейчас',
+    apply: 'Применить',
+    time: 'Время',
+    presets: 'Пресеты',
+    prevPeriodMonth: 'Предыдущий месяц',
+    nextPeriodMonth: 'Следующий месяц',
+    prevPeriodYear: 'Предыдущий год',
+    nextPeriodYear: 'Следующий год',
+    prevPeriodDecade: 'Предыдущее десятилетие',
+    nextPeriodDecade: 'Следующее десятилетие',
+    defaultPresets: {
+      lastWeek: 'Последние 7 дней',
+      lastTwoWeeks: 'Последние 14 дней',
+      lastMonth: 'Последние 30 дней',
+      lastQuarter: 'Последние 90 дней',
+      lastThird: 'Последние 120 дней',
+      lastYear: 'Последний 1 год',
+      lastTwoYears: 'Последние 2 года',
+    },
+  },
   Dropdown: {
     states: {
       notFound: {
