7f057af29e100dc4e7313f50fab431d3cccc4882
Aleksei Belov <anbelov@cloud.ru>
2026-07-21 17:29:08 +0400

    feat(FF-8718): add CollapseVertical and ExpandVertical icons

---
+++ packages/icons/src/components/system/sprite/Interface/S/System/CollapseVertical.tsx
@@ -0,0 +1,9 @@
+// DO NOT EDIT MANUALLY
+import { createSpriteIcon } from '../../../../../../factory/createSpriteIcon';
+const CollapseVerticalSVG = createSpriteIcon({
+  symbolId: 'snack-uikit-system-collapse-vertical',
+  testId: '-collapse-vertical',
+  fallback:
+    '<path d="M6.980 5.000 L 6.461 5.521 9.230 8.290 L 12.000 11.060 14.770 8.290 L 17.540 5.520 17.010 4.990 L 16.480 4.460 14.240 6.700 L 12.000 8.940 9.770 6.710 C 8.543 5.483,7.531 4.480,7.519 4.480 C 7.508 4.480,7.265 4.714,6.980 5.000 M9.230 15.710 L 6.480 18.460 7.010 18.990 L 7.540 19.520 9.770 17.290 L 12.000 15.060 14.230 17.290 L 16.461 19.520 16.990 18.990 L 17.520 18.459 14.770 15.710 C 13.257 14.197,12.011 12.960,12.000 12.960 C 11.989 12.960,10.742 14.198,9.230 15.710 " stroke="none" fill-rule="evenodd"></path>',
+});
+export default CollapseVerticalSVG;
---
+++ packages/icons/src/components/system/sprite/Interface/S/System/ExpandVertical.tsx
@@ -0,0 +1,9 @@
+// DO NOT EDIT MANUALLY
+import { createSpriteIcon } from '../../../../../../factory/createSpriteIcon';
+const ExpandVerticalSVG = createSpriteIcon({
+  symbolId: 'snack-uikit-system-expand-vertical',
+  testId: '-expand-vertical',
+  fallback:
+    '<path d="M9.230 6.710 L 6.480 9.460 7.010 9.990 L 7.540 10.520 9.770 8.290 L 12.000 6.060 14.230 8.290 L 16.461 10.520 16.990 9.990 L 17.520 9.459 14.770 6.710 C 13.257 5.197,12.011 3.960,12.000 3.960 C 11.989 3.960,10.742 5.198,9.230 6.710 M6.980 14.000 L 6.461 14.521 9.230 17.290 L 12.000 20.060 14.770 17.290 L 17.540 14.520 17.010 13.990 L 16.480 13.460 14.240 15.700 L 12.000 17.940 9.770 15.710 C 8.543 14.483,7.531 13.480,7.519 13.480 C 7.508 13.480,7.265 13.714,6.980 14.000 " stroke="none" fill-rule="evenodd"></path>',
+});
+export default ExpandVerticalSVG;
