fb8d860ad84cac7346e0035630882da739e38f40
Nikita Ershov <niershov@cloud.ru>
2026-09-10 12:48:47 +0300

    fix(FF-8873): align notification styles with Figma master

--- packages/uikit-product-notification/src/components/NotificationCard/NotificationCardSkeleton.tsx
+++ packages/uikit-product-notification/src/components/NotificationCard/NotificationCardSkeleton.tsx
@@ -18,13 +18,13 @@ export function NotificationCardSkeleton({ className, ...rest }: NotificationCar
       data-test-id={TEST_IDS.card.skeleton}
       {...extractSupportProps(rest)}
     >
-      <SkeletonText width={140} variant='label' size='s' lines={1} />
+      <SkeletonText width={140} variant='body' size='s' lines={1} />
 
       <div className={styles.notificationCardTitle}>
         <div className={styles.notificationCardTitleIcon}>
-          <SkeletonText width={16} variant='title' size='s' lines={1} />
+          <SkeletonText width={16} variant='label' size='l' lines={1} />
         </div>
-        <SkeletonText width={140} variant='title' size='s' lines={1} />
+        <SkeletonText width={140} variant='label' size='l' lines={1} />
       </div>
 
       <div className={styles.notificationCardContent}>
@@ -33,7 +33,7 @@ export function NotificationCardSkeleton({ className, ...rest }: NotificationCar
 
       <div className={styles.notificationCardFooter}>
         <SkeletonText width={140} variant='body' size='s' lines={1} />
-        <SkeletonText width={140} variant='label' size='s' lines={1} />
+        <SkeletonText width={140} variant='body' size='s' lines={1} />
       </div>
     </div>
   );
